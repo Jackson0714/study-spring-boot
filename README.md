9f351028a9995a31e3e33b70b63cd0ad1d5d72f7
jay.huang <jackson0585@163.com>
2020-03-01 08:45:45 +0800

    Add jdbc notes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-学习SpringBoot的例子
+# 学习SpringBoot的例子
+
+## 01. Spring Boot 入门案例 打印hello world
+
+## 02. Spring Boot 配置
+
+## 03. Spring Boot 日志
+
+##04. Spring Boot Web开发
+
+##05. Spring Boot JDBC开发
+
+## 06. Spring Boot Druid
+
+## 07.Spring Boot MyBatis
+
+##08.Spring Boot JPA
--- spring-boot-05-data-jdbc/src/test/java/com/jackson0714/springboot/Springboot05DataJdbcApplicationTests.java
+++ spring-boot-05-data-jdbc/src/test/java/com/jackson0714/springboot/Springboot05DataJdbcApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jackson0714.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @SpringBootTest
 class Springboot05DataJdbcApplicationTests {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws SQLException {
+		System.out.println("数据源：" + dataSource.getClass());
+
+		Connection connection = dataSource.getConnection();
+		System.out.println("数据库连接：" + connection);
+		connection.close();
 	}
 
 }
